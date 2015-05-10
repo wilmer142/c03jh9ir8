b25abaac008518e082d279f95cd84ac64a20da58
German Escobar <german.escobarc@gmail.com>
2015-05-09 21:48:48 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,53 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Data taken from http://imdb.com/
+
+Actor.create(name: "Elizabeth Olsen", 
+  bio: "Elizabeth Olsen was born and raised in Sherman Oaks, California, the daughter of Jarnette Jarnie (Jones), a personal manager, and David Dave Olsen, a real estate developer and mortgage banker. Elizabeth is the younger sister of twins Mary-Kate Olsen and Ashley Olsen, who became famous as TV and movie stars at an early age",
+  birth_date: "1989-02-16",
+  birth_place: "Sherman Oaks, California, USA",
+  alive: true,
+  image_url: "http://ia.media-imdb.com/images/M/MV5BMjEzMjA0ODk1OF5BMl5BanBnXkFtZTcwMTA4ODM3OQ@@._V1_SY317_CR6,0,214,317_AL_.jpg"
+  )
+
+Actor.create(name: "Robin Williams", 
+  bio: "Robin McLaurin Williams was born on July 21, 1951 in Chicago, Illinois, a great-great-grandson of Mississippi Governor and Senator Anselm J. McLaurin. His mother, Laurie McLaurin (née Janin), was a former model from Mississippi, and his father, Robert Fitzgerald Williams, was a Ford Motor Company executive from Indiana",
+  birth_date: "1951-97-21",
+  birth_place: "Chicago, Illinois, USA",
+  alive: false,
+  death_date: "2014-08-11",
+  death_place: "Chicago, Illinois, USA",
+  image_url: "http://ia.media-imdb.com/images/M/MV5BNTYzMjc2Mjg4MF5BMl5BanBnXkFtZTcwODc1OTQwNw@@._V1_SX214_CR0,0,214,317_AL_.jpg"
+  )
+
+Actor.create(name: "Scarlett Johansson", 
+  bio: "Scarlett Johansson was born in New York City. Her mother, Melanie Sloan, is from an Ashkenazi Jewish family, and her father, Karsten Johansson, is Danish. She has a sister named Vanessa Johansson, a brother named Adrian, and a twin brother named Hunter Johansson born three minutes after her",
+  birth_date: "1984-11-22",
+  birth_place: "New York City, New York, USA",
+  alive: true,
+  image_url: "http://ia.media-imdb.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_SY317_CR23,0,214,317_AL_.jpg"
+  )
+
+Actor.create(name: "Philip Seymour Hoffman", 
+  bio: "Film and stage actor and theater director Philip Seymour Hoffman was born in the Rochester, New York, suburb of Fairport on July 23, 1967. He was the son of Marilyn (Loucks), a lawyer and judge, and Gordon Stowell Hoffman, a Xerox employee, and was mostly of German, Irish, English and Dutch ancestry.",
+  birth_date: "1967-07-23",
+  birth_place: "Fairport, New York, USA",
+  alive: false,
+  death_date: "2014-07-23",
+  death_place: "West Village, Mangattan, New York City, New York, USA",
+  image_url: "http://ia.media-imdb.com/images/M/MV5BMTQ0NTA1NTg3Ml5BMl5BanBnXkFtZTcwNzkxNzgxNw@@._V1_SY317_CR8,0,214,317_AL_.jpg"
+  )
+
+Actor.create(name: "Jared Leto", 
+  bio: "In the vein of musicians-turned-actors, Jared Leto is a very familiar face in recent film history. Although he has always been the lead vocals, rhythm guitar, and songwriter for American band 30 Seconds to Mars, Leto will always be remembered as an accomplished actor for the numerous, challenging projects he has taken in his life.",
+  birth_date: "1971-12-26",
+  birth_place: "Bossier City, Louisiana, USA",
+  alive: true,
+  image_url: "http://ia.media-imdb.com/images/M/MV5BMTczMjUwNDUzMF5BMl5BanBnXkFtZTgwNDA4OTAzMTE@._V1_SX214_CR0,0,214,317_AL_.jpg"
+  )
+
+Actor.create(name: "Robert Downey Jr.", 
+  bio: "Robert Downey Jr. has evolved into one of the most respected actors in Hollywood. With an amazing list of credits to his name, he has managed to stay new and fresh even after over four decades in the business. Downey was born April 4, 1965 in Manhattan, New York, the son of writer, director and filmographer Robert Downey Sr. and actress Elsie Downey",
+  birth_date: "1965-04-04",
+  birth_place: "New York City, New York, USA",
+  alive: true,
+  image_url: "http://ia.media-imdb.com/images/M/MV5BMTAwNjk2NTUyMzleQTJeQWpwZ15BbWU3MDQ2NzQzMTc@._V1_SY317_CR2,0,214,317_AL_.jpg"
+  )
